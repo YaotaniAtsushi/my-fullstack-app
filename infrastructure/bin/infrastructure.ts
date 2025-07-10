7282e729e89289435d611043b11707037ab299d8
yaotani <yaomusic0413@gmail.com>
2025-07-11 01:03:01 +0900

    スタック名変更

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { MainStack } from '../lib/main-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
+new MainStack(app, 'MainStack', {
 
 });
